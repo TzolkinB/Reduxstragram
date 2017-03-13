61858777f362b6ead9fd20e81ef175f0d8925798
Kimberly Bell <kimberly.bell@tma1.com>
2017-03-13 17:39:01 -0400

    Video 8 completed

--- client/components/Main.js
+++ client/components/Main.js
@@ -1,16 +1,17 @@
-import React from 'react';
+import React    from 'react';
 import { Link } from 'react-router';
 
-const Main = React.createClass({
+class Main extends React.Component {
 	render() {
 		return(
 			<div>
 				<h1>
 					<Link to="/">Reduxstagram</Link>
 				</h1>
+        {React.cloneElement(this.props.children, this.props)}
 			</div>
 		)
 	}
-});
+};
 
 export default Main;
--- client/store.js
+++ client/store.js
@@ -1,15 +1,20 @@
-import { createStore, compse } from 'redux';
+import { createStore, compse }  from 'redux';
 import { syncHistoryWithStore } from 'react-router-redux';
-import { browserHistory } from 'react-router';
+import { browserHistory }       from 'react-router';
+import comments                 from './data/comments';
+import posts                    from './data/posts';
 
 // import the root reducer
 import rootReducer from './reducers/index';
 
-import comments from './data/comments';
-import posts from './data/posts';
 
 // create an object for the default data
 const defaultState = {
 	posts, 
 	comments
 };
+
+const store = createStore(rootReducer, defaultState);
+export const history = syncHistoryWithStore(browserHistory, store);
+
+export default store;
