22d89030a91bcf15d07ddcab501e5975d80436c2
Kimberly Bell <kimberly.bell@tma1.com>
2017-03-14 09:08:24 -0400

    Video 12 like button increments likes

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -1,5 +1,20 @@
+// reducers take two things:
+// 1. the action ( what happened )
+// 2. a copy of the current state
+
 function posts(state = [], action) {
-	return state;	
+	const i = action.index;
+	
+	switch(action.type) {
+		case 'INCREMENT_LIKES' :
+			return [
+				...state.slice(0,i), //up until the post we are updating
+				{...state[i], likes: state[i].likes +1}, //post we are updating
+				...state.slice(i + 1), //all posts after the one we are updating
+			]
+		default:
+			return state;	
+	}
 }
 
 export default posts;
